1c38558bb2020b99ede8437627aa0bdecaaca92f
Jose Luis Quintero <20149097@tafe.wa.edu.au>
2026-09-07 21:49:10 +0800

    feat: update User model with new fillable fields and role verification casts

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,22 +11,83 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasRoles;
 
-#[Fillable(['name', 'email', 'password'])]
+#[Fillable([
+    'name',
+    'email',
+    'password',
+    'password_confirmed_at',
+    'email_verified_at',
+    'student_id',
+    'isCurrent',
+    'isAlumni',
+    'isLecturer',
+    'isPartner',
+    'isVerifiedCurrent',
+    'isVerifiedAlumni',
+    'isVerifiedLecturer',
+    'isVerifiedPartner',
+])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, HasRoles, Notifiable;
 
+    public function profile(): HasOne
+    {
+        return $this->hasOne(Profile::class);
+    }
+
+
+    public function selfDeclaredRoleLabel(): ?string
+    {
+        if ($this->isCurrent) {
+            return 'Current Student';
+        }
+
+        if ($this->isAlumni) {
+            return 'Alumni';
+        }
+
+        if ($this->isLecturer) {
+            return 'Lecturer';
+        }
+
+        if ($this->isPartner) {
+            return 'Partner';
+        }
+
+        if ($this->hasRole('General User')) {
+            return 'General User';
+        }
+
+        return null;
+    }
+
+
+
     /**
      * Get the attributes that should be cast.
      *
      * @return array<string, string>
      */
+
+
+
     protected function casts(): array
     {
         return [
             'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+            'student_id' => 'integer',
+            'isCurrent' => 'boolean',
+            'isAlumni' => 'boolean',
+            'isLecturer' => 'boolean',
+            'isPartner' => 'boolean',
+            'isVerifiedCurrent' => 'boolean',
+            'isVerifiedAlumni' => 'boolean',
+            'isVerifiedLecturer' => 'boolean',
+            'isVerifiedPartner' => 'boolean',
             'password' => 'hashed',
         ];
     }
