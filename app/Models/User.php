62da54017d01a09babef5458fbed7bd70e735f5f
Jose Luis Quintero <20149097@tafe.wa.edu.au>
2026-09-08 22:37:51 +0800

    implemnt pint to fix format in user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,8 +6,8 @@ use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasRoles;
 
@@ -39,7 +39,6 @@ class User extends Authenticatable
         return $this->hasOne(Profile::class);
     }
 
-
     public function selfDeclaredRoleLabel(): ?string
     {
         if ($this->isCurrent) {
@@ -70,7 +69,6 @@ class User extends Authenticatable
      *
      * @return array<string, string>
      */
-
     protected function casts(): array
     {
         return [
